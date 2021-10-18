fa7082f771255206c8ad9469994f1f2ef9204232
VladKostik <86021794+VladKostik@users.noreply.github.com>
2021-10-18 20:54:12 +0300

    Delete selenium_4 directory

--- selenium_4/tests/test_nova_poshta_3.py
+++
@@ -1,9 +0,0 @@
-def test_cookies(cookie):
-    cookie._add_one('test cookie', 'test cookie value')
-    print("\nCOOKIES:", cookie._get_one('test cookie'))
-    print("\nCOOKIES:", cookie._get_all())
-
-def test_local_storage(local_storage):
-    local_storage._add_one('name', 'dataaaaa')
-    print("\nLOCAL STORAGE: ", local_storage._get_one_by_name('name'))
-    print("\nLOCAL STORAGE: ", local_storage._get_all())
